120751fa3699cc5cdc70eb406e015cb0f5cda6fa
tushar-gleecus <tushar.canchi@gleecus.com>
2025-06-08 16:43:15 +0530

    Use only Postgres for Railway

--- realestate/realestate/settings.py
+++ realestate/realestate/settings.py
@@ -97,12 +97,6 @@ REST_FRAMEWORK = {
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
